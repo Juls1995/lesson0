15cfb68e882ae4ab01456ee5e194a83b5a0ace42
Juls1995 <juliajdanova95@gmail.com>
2024-06-10 17:27:52 +0300

    Add files via upload

---
+++ resolve0.py
@@ -0,0 +1,22 @@
+a = 'мама мыла раму'
+print(len(a))  # len -  подсчёт символов
+
+first = 3
+second = 2
+summa = (first + second)
+diff = (first - second)
+print(summa, diff)
+
+third = 1
+mean = ((first + second + third)//3)
+print(mean)
+
+first_string = 'вторник'
+second_string = 'понедельник,'
+print(second_string, first_string)
+
+a = 3
+b = 4
+c = 5
+f = (a * b) + (a * c)
+print(f**3/2)
